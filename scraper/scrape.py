4216c43820f99ef912cca915ebf1272cf0675549
Sean Becker <sean.becker15@gmail.com>
2024-09-12 23:17:52 -0500

    refactor

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -6,14 +6,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
-from dotenv import load_dotenv
 from time import sleep
-from openai import OpenAI
 
-# client = OpenAI(
-# #   organization='org-KmpePDQ8iqEKTtaUOa5AZtMG',
-# #   project='proj_nDIJ1BDWc0boD4owh9O7WOp6',
-# )
+login_page_url = 'https://www.cbssports.com/login?masterProductId=40482&product_abbrev=opm&show_opts=1&xurl=https%3A%2F%2Fpicks.cbssports.com%2Ffootball%2Fpickem%2Fpools%2Fkbxw63b2geytimrqgmyts%253D%253D%253D%2Fstandings%2Fweekly'
+
+
+def __navigate_login(driver, max_wait_time, email: str, password: str) -> int:
+    driver.get(login_page_url)
+    if email == None or len(email) == 0:
+        print("Email not found. Make sure .env file is configured correctly.")
+        return 1
+    if password == None or len(password) == 0:
+        print("Password not found. Make sure .env file is configured correctly.")
+        return 1
+    try:
+        userid_el = WebDriverWait(driver, max_wait_time).until(
+            EC.presence_of_element_located((By.NAME, 'email')))
+        userid_el.send_keys(email)
+        password_el = WebDriverWait(driver, max_wait_time).until(
+            EC.presence_of_element_located((By.NAME, 'password')))
+        password_el.send_keys(password)
+        button_el = WebDriverWait(driver, max_wait_time).until(
+            EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Continue')]")))
+        print('Logging in in 5 seconds...')
+        sleep(5)
+        button_el.click()
+    except TimeoutException:
+        print("It took too much time to load specified elements.")
+
+
+def __navigate_week_standings(driver, max_wait_time, curr_week_number, target_week_number) -> any:
+    # Search for div with text "Week X" and click on it to open menu
+    week_div = WebDriverWait(driver, max_wait_time).until(
+        EC.presence_of_element_located((By.XPATH, f"//div[contains(text(), 'Week {curr_week_number}')]")))
+    week_div.click()
+    # Search for li with text "Week Y" and click on it to navigate to the target week
+    target_week_li = WebDriverWait(driver, max_wait_time).until(
+        EC.presence_of_element_located((By.XPATH, f"//li[contains(text(), 'Week {target_week_number}')]")))
+    target_week_li.click()
+
 
 class Row:
     name = ""
@@ -34,74 +65,7 @@ class Row:
         return csv
 
 
-def login(driver, email: str, password: str, max_time) -> int:
-    url = 'https://www.cbssports.com/login?masterProductId=40482&product_abbrev=opm&show_opts=1&xurl=https%3A%2F%2Fpicks.cbssports.com%2Ffootball%2Fpickem%2Fpools%2Fkbxw63b2geytimrqgmyts%253D%253D%253D%2Fstandings%2Fweekly'
-    driver.get(url)
-
-    if email == None or len(email) == 0:
-        print("Email not found. Make sure .env file is configured correctly.")
-        return 1
-
-    if password == None or len(password) == 0:
-        print("Password not found. Make sure .env file is configured correctly.")
-        return 1
-
-    try:
-        userid_el = WebDriverWait(driver, max_time).until(
-            EC.presence_of_element_located((By.ID, 'name')))
-        userid_el.send_keys(email)
-
-        
-        password_el = WebDriverWait(driver, max_time).until(
-            EC.presence_of_element_located((By.ID, 'password')))
-        password_el.send_keys(password)
-
-        
-        button_el = WebDriverWait(driver, max_time).until(
-            EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Continue')]")))
-
-        print('Logging in in 5 seconds...')
-        sleep(5)
-        button_el.click()
-    except TimeoutException:
-        print("It took too much time to load specified elements.")
-    
-icon_check_svg_path = "M12 22c5.5 0 10-4.5 10-10S17.5 2 12 2 2 6.5 2 12s4.5 10 10 10zm-1-5.2c-.4 0-.8-.2-1.1-.6l-2.2-2.6c-.2-.3-.3-.5-.3-.8 0-.6.5-1.1 1.1-1.1.3 0 .6.1.9.4l1.6 2 3.7-5.8c.3-.4.6-.6.9-.6.6 0 1.1.4 1.1 1 0 .2-.1.5-.3.7L12 16.2c-.3.3-.6.6-1 .6z"
-icon_x_svg_path = "M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm4.3 14.3c-.4.4-1 .4-1.4 0L12 13.4l-2.9 2.9c-.4.4-1 .4-1.4 0-.4-.4-.4-1 0-1.4l2.9-2.9-2.9-2.9c-.4-.4-.4-1 0-1.4.4-.4 1-.4 1.4 0l2.9 2.9 2.9-2.9c.4-.4 1-.4 1.4 0 .4.4.4 1 0 1.4L13.4 12l2.9 2.9c.4.4.4 1 0 1.4z"
-
-def get_cell_type(cell: WebElement) -> str:
-    # Check if cell has an svg indicating a win or loss
-    try:
-        path = cell.find_element(By.TAG_NAME, "path")
-        if path:
-            path_d = path.get_attribute("d")
-            if path_d == icon_check_svg_path:
-                return "win"
-            elif path_d == icon_x_svg_path:
-                return "loss"
-    except:
-        pass
-
-    return "unknown"
-
-        
-
-def get_results_for_week(driver, max_wait_time, current_week, target_week) -> any:
-    # url = "https://picks.cbssports.com/football/pickem/pools/kbxw63b2geytimrqgmyts%3D%3D%3D/standings/weekly"
-    # driver.get(url)
-    
-    # Search for div with text "Week X" and click on it to open menu
-    week_div = WebDriverWait(driver, max_wait_time).until(
-        EC.presence_of_element_located((By.XPATH, f"//div[contains(text(), 'Week {current_week}')]")))
-    week_div.click()
-
-    # Search for li with text "Week Y" and click on it to navigate to the target week
-    target_week_li = WebDriverWait(driver, max_wait_time).until(
-        EC.presence_of_element_located((By.XPATH, f"//li[contains(text(), 'Week {target_week}')]")))
-    target_week_li.click()
-
-    sleep(5)
-
+def __scrape_week_standings(driver, max_wait_time) -> any:
     # Search for a table with aria-label "Weekly Standings" and get all rows
     table = WebDriverWait(driver, max_wait_time).until(
         EC.presence_of_element_located((By.XPATH, "//table[@aria-label='Weekly Standings']")))
@@ -127,7 +91,7 @@ def get_results_for_week(driver, max_wait_time, current_week, target_week) -> an
         wins = 0
         losses = 0
         for cell in cells[3:]:
-            cell_type = get_cell_type(cell)
+            cell_type = __get_cell_type(cell)
             if cell_type == "win":
                 wins += 1
             elif cell_type == "loss":
@@ -137,42 +101,64 @@ def get_results_for_week(driver, max_wait_time, current_week, target_week) -> an
         row_obj.name = player_name
         row_obj.results = [player_points, wins, losses]
         parsed_rows.append(row_obj)
-        print(f"Player: {player_name}, Points: {player_points}, Wins: {wins}, Losses: {losses}")
+        print(
+            f"Player: {player_name}, Points: {player_points}, Wins: {wins}, Losses: {losses}")
 
     return parsed_rows
-    
-    # Send the table body to ChatGPT for parsing
-    # table_html = table_body.get_attribute("outerHTML")
-    # print(table_html)
-    # completion = client.chat.completions.create(
-    #     model="gpt-4o",
-    #     messages=[
-    #         {"role": "system", "content": "You have a table representing the weekly standings for a confidence pool. Parse the table and return the results as JSON using the schema provided."},
-    #     ]
-    # )
-    # print(completion)
 
-def print_most_wins(results):
+
+icon_check_svg_path = "M12 22c5.5 0 10-4.5 10-10S17.5 2 12 2 2 6.5 2 12s4.5 10 10 10zm-1-5.2c-.4 0-.8-.2-1.1-.6l-2.2-2.6c-.2-.3-.3-.5-.3-.8 0-.6.5-1.1 1.1-1.1.3 0 .6.1.9.4l1.6 2 3.7-5.8c.3-.4.6-.6.9-.6.6 0 1.1.4 1.1 1 0 .2-.1.5-.3.7L12 16.2c-.3.3-.6.6-1 .6z"
+icon_x_svg_path = "M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm4.3 14.3c-.4.4-1 .4-1.4 0L12 13.4l-2.9 2.9c-.4.4-1 .4-1.4 0-.4-.4-.4-1 0-1.4l2.9-2.9-2.9-2.9c-.4-.4-.4-1 0-1.4.4-.4 1-.4 1.4 0l2.9 2.9 2.9-2.9c.4-.4 1-.4 1.4 0 .4.4.4 1 0 1.4L13.4 12l2.9 2.9c.4.4.4 1 0 1.4z"
+
+
+def __get_cell_type(cell: WebElement) -> str:
+    # Check if cell has an svg indicating a win or loss
+    try:
+        path = cell.find_element(By.TAG_NAME, "path")
+        if path:
+            path_d = path.get_attribute("d")
+            if path_d == icon_check_svg_path:
+                return "win"
+            elif path_d == icon_x_svg_path:
+                return "loss"
+    except:
+        pass
+
+    return "unknown"
+
+
+def __print_csv(results):
+    csv = "Name,Points,Wins,Losses\n"
+    for row in results:
+        csv += row.csv() + "\n"
+    print(csv)
+
+
+def __print_most_wins(results):
     max_wins = 0
     for row in results:
         if row.results[1] > max_wins:
             max_wins = row.results[1]
-    players_with_max_wins = [row.name for row in results if row.results[1] == max_wins]
+    players_with_max_wins = [
+        row.name for row in results if row.results[1] == max_wins]
     print(f"Most wins for the week: {max_wins}")
     print(f"Players with the most wins: {', '.join(players_with_max_wins)}")
 
-def print_most_points(results):
+
+def __print_most_points(results):
     max_points = 0
     for row in results:
         curr_row_points = int(row.results[0])
         if curr_row_points > max_points:
             max_points = curr_row_points
-    players_with_max_points = [row.name for row in results if int(row.results[0]) == max_points]
+    players_with_max_points = [
+        row.name for row in results if int(row.results[0]) == max_points]
     print(f"Most points for the week: {max_points}")
-    print(f"Players with the most points: {', '.join(players_with_max_points)}")
+    print(
+        f"Players with the most points: {', '.join(players_with_max_points)}")
 
-def main() -> int:
-    load_dotenv()
+
+def run_scraper(curr_week_number: int, target_week_number: int) -> int:
     email = os.getenv("EMAIL")
     password = os.getenv("PASSWORD")
 
@@ -181,25 +167,13 @@ def main() -> int:
 
     driver = webdriver.Chrome(options=chrome_options)
 
-    login(driver, email, password, max_wait_time)
-
-    print('Logged in successfully.')
-    print("Getting results in 5 seconds...")
+    __navigate_login(driver, max_wait_time, email, password)
     sleep(5)
 
-    # Wait for the page to load the text "3 G'S FOOTBALL POOL"
-    # WebDriverWait(driver, max_time).until(
-    #     EC.presence_of_element_located((By.XPATH, "//h1[contains(text(), \"3 G'S FOOTBALL POOL\")]")))
+    __navigate_week_standings(driver, max_wait_time, curr_week_number, target_week_number)
+    sleep(5)
 
-    results = get_results_for_week(driver, max_wait_time, 2, 1)
-    csv = "Name,Points,Wins,Losses\n"
-    for row in results:
-        csv += row.csv() + "\n"
-
-    print(csv)
-    print_most_wins(results)
-    print_most_points(results)
-
-
-if __name__ == '__main__':
-    main()
+    results = __scrape_week_standings(driver, max_wait_time)
+    __print_csv(results)
+    __print_most_wins(results)
+    __print_most_points(results)
