4aa956372152ce0b9eae82f411d98b9791db590d
Sean Becker <sean.becker15@gmail.com>
2024-10-15 09:15:48 -0500

    update schedule

--- scripts/schedule-task.sh
+++ scripts/schedule-task.sh
@@ -1,2 +1,3 @@
+launchctl unload ~/Library/LaunchAgents/com.cbs-sports.scraper.plist
 cp schedule/com.cbs-sports.scraper.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/com.cbs-sports.scraper.plist
