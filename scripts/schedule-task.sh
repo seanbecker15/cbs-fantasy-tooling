e0696600afc1f3cda8b01fe8d977a0fbef3d2ba7
Sean Becker <sean.becker15@gmail.com>
2025-12-09 21:51:00 -0600

    Fix plist

--- scripts/schedule-task.sh
+++ scripts/schedule-task.sh
@@ -1,3 +1,4 @@
 launchctl unload ~/Library/LaunchAgents/com.cbs-sports.scraper.plist
 cp schedule/com.cbs-sports.scraper.plist ~/Library/LaunchAgents
+chmod 644 ~/Library/LaunchAgents/com.cbs-sports.scraper.plist
 launchctl load ~/Library/LaunchAgents/com.cbs-sports.scraper.plist
