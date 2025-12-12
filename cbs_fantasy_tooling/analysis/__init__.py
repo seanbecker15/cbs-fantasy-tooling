917024f3307ec5796cd1c4d34df66ee0f8ad801a
Sean Becker <sean.becker15@gmail.com>
2025-12-12 00:09:34 -0600

    Move league contrarian picks to new location

--- cbs_fantasy_tooling/analysis/__init__.py
+++ cbs_fantasy_tooling/analysis/__init__.py
@@ -5,7 +5,7 @@ Provides confidence pool strategy analysis and simulation capabilities.
 
 from .monte_carlo import run_strategy_simulation
 from .competitor_intelligence import analyze_competitors
-from .visualization.contrarian_picks import analyze_contrarian_picks
+from .league_contrarian_picks import analyze_contrarian_picks
 from .win_scenario import analyze_win_scenarios, analyze_win_leaderboard
 from .user_win_percentage import analyze_user_win_percentage
 
--- cbs_fantasy_tooling/analysis/visualization/__init__.py
+++
@@ -1,5 +0,0 @@
-from .contrarian_picks import analyze_contrarian_picks
-
-__all__ = [
-    "analyze_contrarian_picks",
-]
